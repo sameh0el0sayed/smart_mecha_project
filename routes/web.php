1af19a3b8f0e1a1acaf1604fe8c1fefc1e6a0a0b
sameh0el0sayed <samehsr.work@gmail.com>
2022-06-08 00:29:44 +0300

    save

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\UserController;
 
  /*
 |--------------------------------------------------------------------------
@@ -16,11 +17,24 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+// Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
- Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::view('/', 'Ecommerce.Home')->name('main_home');
 
-Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin');
-Route::get('/workshop', [App\Http\Controllers\WorkshopController::class, 'index'])->name('workshop');
-Route::get('/booking', [App\Http\Controllers\AppointmentController::class, 'index'])->name('booking');
+
+Route::prefix('user')->name('user.')->group(function(){
+    Route::middleware(['guest'])->group(function(){
+        Route::view('/login', 'auth.login')->name('login');
+        Route::view('/register', 'auth.register')->name('register');
+    });
+
+    Route::middleware(['auth'])->group(function(){
+        Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin');
+        Route::get('/workshop', [App\Http\Controllers\WorkshopController::class, 'index'])->name('workshop');
+        Route::get('/booking', [App\Http\Controllers\AppointmentController::class, 'index'])->name('booking');
+
+    });
+
+});
